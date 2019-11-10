f8ff4ebb0b085150ec8ef8690dc7ef8ffa26a138
kakastg <kakastg9@gmail.com>
2019-11-10 20:19:26 +0100

    Another funny project - MVC ASP.NET C#

---
+++ Aukcje/Controllers/AukcjeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Aukcje.Models;
+
+namespace Aukcje.Controllers
+{
+    public class AukcjeController : Controller
+    {
+        // GET: Aukcje
+        public ActionResult Index()
+        {
+            List<Aukcja> aukcje = new List<Aukcja>()
+            {
+                new Aukcja("Latarka", 12.99m, "Szef wszystkich szefów", 7),
+                new Aukcja("Młotek", 8, "Jan III Sobieski", 14),
+                new Aukcja("Turbomotopompka", 99.99m, "Krzysztof Jarzyna", 7)
+            };
+            return View(aukcje);
+        }
+    }
+}
---
+++ Aukcje/Models/Aukcja.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Aukcje.Models
+{
+    public class Aukcja
+    {
+        public static int liczbaAukcji = 0;
+        public int aukcjaID { get; set; }
+        public String przedmiot { get; set; }
+        public decimal cenaWywolawcza { get; set; }
+        public List<decimal> ceny { get; set; }
+        public DateTime dataStart { get; set; }
+        public DateTime dataZakonczenia { get; set; }
+        public String sprzedajacy { get; set; }
+        public String status { get; set; }
+
+        public Aukcja(String przedmiot, decimal cenaWywolawcza,String sprzedajacy, int czasTrwania)
+        {
+            this.aukcjaID = liczbaAukcji++;
+            this.przedmiot = przedmiot;
+            this.cenaWywolawcza = cenaWywolawcza;
+            this.ceny = new List<decimal>();
+            this.dataStart = DateTime.Now;
+            this.dataZakonczenia = DateTime.Now.AddDays(czasTrwania);
+            this.sprzedajacy = sprzedajacy;
+            this.status = "aktywna";
+
+        }
+        
+    }
+}
